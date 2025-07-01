be4b8aa8005b7af156376e54148fd7d57511c478
vovavish <vovavishniakov856@gmail.com>
2025-07-02 02:46:52 +0300

    feat: 103

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+import { createRoot } from "react-dom/client";
+import { Header } from "./Header";
+import { Main } from "./MainContent";
+import "./index.css";
+
+const root = createRoot(document.getElementById("app"));
+root.render(
+  <>
+    <Header />
+    <Main />
+  </>
+);
---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  base: '/shri2025-task/',
+  plugins: [react()],
+});
