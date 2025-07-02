06a6e5277b4e4e481928c4686ad997d295309ff9
vovavish <vovavishniakov856@gmail.com>
2025-07-02 22:06:46 +0300

    feat: finish 120

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState, memo } from "react";
+import { useRef, useEffect, useState } from "react";
 
 const Event = function Event(props) {
   return (
@@ -18,108 +18,6 @@ const Event = function Event(props) {
   );
 };
 
-const GeneralInfo = memo(function GeneralInfo() {
-  return (
-    <section className="section main__general">
-      <h2 className="section__title section__title-header section__main-title">
-        Главное
-      </h2>
-      <div className="hero-dashboard">
-        <div className="hero-dashboard__primary">
-          <h3 className="hero-dashboard__title">Привет, Геннадий!</h3>
-          <p className="hero-dashboard__subtitle">
-            Двери и окна закрыты, сигнализация включена.
-          </p>
-          <ul className="hero-dashboard__info">
-            <li className="hero-dashboard__item">
-              <div className="hero-dashboard__item-title">Дома</div>
-              <div className="hero-dashboard__item-details">
-                +23
-                <span className="a11y-hidden">°</span>
-              </div>
-            </li>
-            <li className="hero-dashboard__item">
-              <div className="hero-dashboard__item-title">За окном</div>
-              <div className="hero-dashboard__item-details">
-                +19
-                <span className="a11y-hidden">°</span>
-                <div
-                  className="hero-dashboard__icon hero-dashboard__icon_rain"
-                  role="img"
-                  aria-label="Дождь"
-                ></div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <ul className="hero-dashboard__schedule">
-          <Event
-            icon="temp"
-            iconLabel="Температура"
-            title="Philips Cooler"
-            subtitle="Начнет охлаждать в 16:30"
-          />
-          <Event
-            icon="light"
-            iconLabel="Освещение"
-            title="Xiaomi Yeelight LED Smart Bulb"
-            subtitle="Включится в 17:00"
-          />
-          <Event
-            icon="light"
-            iconLabel="Освещение"
-            title="Xiaomi Yeelight LED Smart Bulb"
-            subtitle="Включится в 17:00"
-          />
-        </ul>
-      </div>
-    </section>
-  );
-});
-
-const MainScripts = memo(function MainScripts() {
-  return (
-    <section className="section main__scripts">
-      <h2 className="section__title section__title-header">
-        Избранные сценарии
-      </h2>
-
-      <ul className="event-grid">
-        <Event
-          slim={true}
-          icon="light2"
-          iconLabel="Освещение"
-          title="Выключить весь свет в доме и во дворе"
-        />
-        <Event
-          slim={true}
-          icon="schedule"
-          iconLabel="Расписание"
-          title="Я ухожу"
-        />
-        <Event
-          slim={true}
-          icon="light2"
-          iconLabel="Освещение"
-          title="Включить свет в коридоре"
-        />
-        <Event
-          slim={true}
-          icon="temp2"
-          iconLabel="Температура"
-          title="Набрать горячую ванну"
-          subtitle="Начнётся в 18:00"
-        />
-        <Event
-          slim={true}
-          icon="temp2"
-          iconLabel="Температура"
-          title="Сделать пол тёплым во всей квартире"
-        />
-      </ul>
-    </section>
-  );
-});
 const TABS = {
   all: {
     title: "Все",
@@ -293,65 +191,60 @@ export function Main() {
     return null;
   }
   return (
-    <main className="main">
-      <GeneralInfo />
-      <MainScripts />
+    <section>
+      <div className="section__title">
+        <h2 className="section__title-header">Избранные устройства</h2>
 
-      <section className="section main__devices">
-        <div className="section__title">
-          <h2 className="section__title-header">Избранные устройства</h2>
+        <select
+          className="section__select"
+          defaultValue="all"
+          onInput={onSelectInput}
+        >
+          {TABS_KEYS.map((key) => (
+            <option key={key} value={key}>
+              {TABS[key].title}
+            </option>
+          ))}
+        </select>
 
-          <select
-            className="section__select"
-            defaultValue="all"
-            onInput={onSelectInput}
-          >
-            {TABS_KEYS.map((key) => (
-              <option key={key} value={key}>
-                {TABS[key].title}
-              </option>
-            ))}
-          </select>
+        <ul role="tablist" className="section__tabs">
+          {TABS_KEYS.map((key) => (
+            <li
+              key={key}
+              role="tab"
+              aria-selected={key === activeTab ? "true" : "false"}
+              tabIndex={key === activeTab ? "0" : undefined}
+              className={
+                "section__tab" +
+                (key === activeTab ? " section__tab_active" : "")
+              }
+              id={`tab_${key}`}
+              aria-controls={`panel_${key}`}
+              onClick={() => setActiveTab(key)}
+            >
+              {TABS[key].title}
+            </li>
+          ))}
+        </ul>
+      </div>
 
-          <ul role="tablist" className="section__tabs">
-            {TABS_KEYS.map((key) => (
-              <li
-                key={key}
-                role="tab"
-                aria-selected={key === activeTab ? "true" : "false"}
-                tabIndex={key === activeTab ? "0" : undefined}
-                className={
-                  "section__tab" +
-                  (key === activeTab ? " section__tab_active" : "")
-                }
-                id={`tab_${key}`}
-                aria-controls={`panel_${key}`}
-                onClick={() => setActiveTab(key)}
-              >
-                {TABS[key].title}
-              </li>
+      <div className="section__panel-wrapper" ref={ref}>
+        <div
+          role="tabpanel"
+          className={"section__panel"}
+          id={`panel_${activeTab}`}
+          aria-labelledby={`tab_${activeTab}`}
+        >
+          <ul className="section__panel-list">
+            {TABS[activeTab].items.map((item, index) => (
+              <Event key={index} {...item} />
             ))}
           </ul>
         </div>
-
-        <div className="section__panel-wrapper" ref={ref}>
-          <div
-            role="tabpanel"
-            className={"section__panel"}
-            id={`panel_${activeTab}`}
-            aria-labelledby={`tab_${activeTab}`}
-          >
-            <ul className="section__panel-list">
-              {TABS[activeTab].items.map((item, index) => (
-                <Event key={index} {...item} />
-              ))}
-            </ul>
-          </div>
-          {hasRightScroll && (
-            <div className="section__arrow" onClick={onArrowCLick}></div>
-          )}
-        </div>
-      </section>
-    </main>
+        {hasRightScroll && (
+          <div className="section__arrow" onClick={onArrowCLick}></div>
+        )}
+      </div>
+    </section>
   );
 }
